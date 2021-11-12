bbb141589be299f069cc6751306ca59c2a931aa2
Rayhan <rayhanchy119gmail.com>
2021-11-12 11:25:02 +0600

    code added

--- src/Pages/PlaceOrder/OrderModal/OrderModal.js
+++ src/Pages/PlaceOrder/OrderModal/OrderModal.js
@@ -1,46 +1,110 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Col, Form, Modal, Row } from "react-bootstrap";
+import swal from "sweetalert";
+
 import useAuth from "../../../Hooks/useAuth";
 import "./OrderModal.css";
+import { useHistory } from "react-router";
 
 const OrderModal = ({ show, handleClose, data }) => {
+  const { img, name } = data;
+  const { users } = useAuth();
+  const history = useHistory();
+  const redirect = "/home";
+
+  const initialInfo = {
+    customerName: users.displayName,
+    email: users.email,
+    phone: "",
+  };
+  const [oderInfo, setOrderInfo] = useState(initialInfo);
+  //collect data from input field
+  const handleOnBlur = (e) => {
+    const field = e.target.name;
+    const value = e.target.value;
+    const newInfo = { ...oderInfo };
+    newInfo[field] = value;
+    setOrderInfo(newInfo);
+  };
   const handleModalSubmit = (e) => {
     e.preventDefault();
-    handleClose();
-    alert("successfully submitted");
+    // swal("Good job!", "You Successfully Order your Drone", "success");
+    //swal("Good job!", "You Successfully Order your Drone", "success");
+
+    // history.push(redirect);
+    //order
+    const orders = {
+      ...oderInfo,
+      productImg: img,
+      productName: name,
+    };
+    console.log(orders);
+    //post order to the server
+
+    fetch("http://localhost:5000/orders", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(orders),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          swal("Good job!", "You Successfully Order your Drone", "success");
+          handleClose();
+          history.push(redirect);
+        }
+      });
   };
-  const { users } = useAuth();
 
   return (
     <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
-      <Modal.Header>
-        <Modal.Title className="text-primary"></Modal.Title>
-      </Modal.Header>
+      <div className="modal-header d-flex justify-content-center">
+        <img src={data.img} alt="" />
+        <h5>{data.name}</h5>
+      </div>
+
       <Modal.Body>
         <Form onSubmit={handleModalSubmit}>
           <Row className="mb-3 modal-form">
             <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Control disabled type="text" />
+              <Form.Control
+                defaultValue={users.displayName}
+                type="text"
+                name="customerName"
+                onBlur={handleOnBlur}
+              />
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridPassword">
-              <Form.Control disabled type="text" />
+              <Form.Control
+                defaultValue={users.email}
+                type="email"
+                name="email"
+                onBlur={handleOnBlur}
+              />
             </Form.Group>
             <Form.Group as={Col} controlId="formGridPassword">
-              <Form.Control disabled type="email" placeholder={users.email} />
+              <Form.Control
+                name="phone"
+                onBlur={handleOnBlur}
+                type="number"
+                placeholder="Your Phone"
+                required
+              />
             </Form.Group>
             <Form.Group as={Col} controlId="formGridPassword">
-              <Form.Control type="number" placeholder="Your Phone" />
-            </Form.Group>
-            <Form.Group as={Col} controlId="formGridPassword">
-              <Form.Control disabled type="text" />
+              <Form.Control
+                disabled
+                placeholder={`${data.price} $`}
+                type="text"
+              />
             </Form.Group>
           </Row>
 
           <div className="modal-submit-btn">
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
+            <Button type="submit">Confirm Order</Button>
           </div>
         </Form>
       </Modal.Body>
